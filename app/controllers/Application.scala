b0589cf8c613b163dbceddd9e345330a1e04e65f
bleezmo <joshuarogers10@gmail.com>
2013-01-27 13:56:59 -0500

    added mailer

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,9 +2,34 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
+import com.typesafe.plugin._
 
 object Application extends Controller {
-  
+
   def index = Assets.at("/public","index.html");
+  def postMail = Action(parse.urlFormEncoded) { request =>
+    request.body.get("name") match {
+      case Some(name :: _) => request.body.get("email") match {
+        case Some(email :: _) => request.body.get("message") match {
+          case Some(message :: _) => {
+            sendMail(name,email,message)
+            Ok
+          }
+          case None => BadRequest("message missing")
+        }
+        case None => BadRequest("email missing")
+      }
+      case None => BadRequest("name missing")
+    }
+  }
+
+  private def sendMail(name:String,email:String,message:String) = {
+    val mail = use[MailerPlugin].email
+    mail.setSubject("karencecilia.com message from "+name)
+    mail.addRecipient("infokarencecilia@gmail.com")
+    mail.addFrom(email)
+    mail.send(name+" has sent you a message:\n"+message)
+  }
   
 }
