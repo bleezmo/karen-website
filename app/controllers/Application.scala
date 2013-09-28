31affe8f8225e3621ef4e2e17d1f3967820f1073
bleezmo <joshuarogers10@gmail.com>
2013-09-28 14:48:11 -0400

    updated play to 2.1. added routes for text messaging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,11 +16,11 @@ object Application extends Controller {
             sendMail(name,email,message)
             Ok
           }
-          case None => BadRequest("message missing")
+          case _ => BadRequest("message missing")
         }
-        case None => BadRequest("email missing")
+        case _ => BadRequest("email missing")
       }
-      case None => BadRequest("name missing")
+      case _ => BadRequest("name missing")
     }
   }
 
@@ -30,6 +30,23 @@ object Application extends Controller {
     mail.addRecipient("infokarencecilia@gmail.com")
     mail.addFrom(email)
     mail.send(name+" has sent you a message:\n\n"+message)
+  }
+  def receiveText = Action {request =>
+    Logger.logger.info("sending email with text:\n"+request.uri)
+    val mail = use[MailerPlugin].email
+    mail.setSubject("Received a text message")
+    mail.addRecipient("joshuarogers10@gmail.com")
+    mail.addFrom("infokarencecilia@gmail.com")
+    mail.send("the request uri received from karen's website: \n"+request.uri)
+    Ok
+  }
+  def receiveTextWithBody = Action {request =>
+    val mail = use[MailerPlugin].email
+    mail.setSubject("Received a text message")
+    mail.addRecipient("joshuarogers10@gmail.com")
+    mail.addFrom("infokarencecilia@gmail.com")
+    mail.send("the request uri received from karen's website: \n"+request.uri+" with a post message body: \n"+request.body.toString)
+    Ok
   }
   
 }
--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,10 +8,10 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.typesafe" %% "play-plugins-mailer" % "2.0.4"
+      "com.typesafe" %% "play-plugins-mailer" % "2.1-RC2"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
     )
 
